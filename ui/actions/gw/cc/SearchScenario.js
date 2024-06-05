0660ee011c95a9a585245d3da6fafef9d652dbb9
ramya <ramya.muralidharan@valuemomentum.com>
2024-06-05 23:03:23 +0530

    removed extra lines

--- ui/actions/gw/cc/SearchScenario.js
+++ ui/actions/gw/cc/SearchScenario.js
@@ -1,12 +1,9 @@
 import { SearchTabBar } from "../../../pages/gw/generated/claimsolutions/pages/navigation/tabBar/SearchTabBar"
 import { SimpleClaimSearch } from "../../../pages/gw/generated/claimsolutions/pages/search/claimSearchesGroup/SimpleClaimSearch"
-import { ClaimSummary_Ext } from "./scenarioPages/claim/claimSummaryGroup/ClaimSummary_Ext"
 import { t } from "testcafe"
-import { searchTableRecord } from "../../../util/gw/helper"
 
 const searchTabBar = new SearchTabBar()
 const simpleClaimSearch = new SimpleClaimSearch()
-const claimSummary_Ext = new ClaimSummary_Ext()
 
 export class SearchScenario {
     async claimSimpleSearch() {
