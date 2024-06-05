77bcb995a0bf7efe6de85ab23510eb7c92f12153
ramya <ramya.muralidharan@valuemomentum.com>
2024-06-05 22:50:20 +0530

    viewClaim_cc

--- ui/actions/gw/cc/ViewClaimSummaryScenario.js
+++ ui/actions/gw/cc/ViewClaimSummaryScenario.js
@@ -1,13 +1,17 @@
 import { ClaimStatus } from "../../../../ui/pages/gw/generated/claimsolutions/pages/claim/claimSummaryGroup/ClaimStatus";
 import { ClaimSummary_Ext } from "./scenarioPages/claim/claimSummaryGroup/ClaimSummary_Ext";
+import {ClaimMenuLinks} from '../../../../ui/pages/gw/generated/claimsolutions/pages/navigation/menuLinks/ClaimMenuLinks'
 import { t } from 'testcafe'
+import world from "../../../util/gw/world";
 
 const claimStatus = new ClaimStatus()
+const claimMenuLinks = new ClaimMenuLinks();
 const claimSummary_Ext = new ClaimSummary_Ext()
 
 export class ViewClaimSummaryScenario {
-    async viewClaimSummary() {
-        await t.expect(claimStatus.claimStatusClaimStatus.component.textContent).eql('Open')
+    async verifyClaimStatus() {
+        await claimMenuLinks.claim_ClaimSummaryGroupClaimSummaryGroup_ClaimStatus.click()
+        await t.expect(claimStatus.claimStatusClaimStatus.component.textContent).eql(world.dataMap.get('ClaimStatus'))
     }
 
     async verifySummaryHeader() {
