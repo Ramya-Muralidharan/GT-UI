e80bf191d3eb29a17671d3aa3d6dd02665fc9e31
ramya <ramya.muralidharan@valuemomentum.com>
2024-06-21 18:25:30 +0530

    feature file refactor

--- ui/actions/gw/pc/CoverageScenario/CoverageHelper.js
+++ ui/actions/gw/pc/CoverageScenario/CoverageHelper.js
@@ -3,6 +3,7 @@ import world from "../../../../util/gw/world"
 
 const lOBWizardStepGroupSubmissionWizard_Ext = new LOBWizardStepGroupSubmissionWizard_Ext()
 
+//Function to check and uncheck coveareg checkbox
 export async function selectCoverage(coverageName) {
     if (world.coverageDataMap.get(coverageName) == 'check' || world.coverageDataMap.get(coverageName) == 'update') {
         if (!(await lOBWizardStepGroupSubmissionWizard_Ext[coverageName].isChecked()))
@@ -12,11 +13,13 @@ export async function selectCoverage(coverageName) {
         await lOBWizardStepGroupSubmissionWizard_Ext[coverageName].click()
 }
 
+//Fucntion to type in coverage input box
 export async function enterInputField(fieldName) {
     if (world.coverageDataMap.has(fieldName))
         await lOBWizardStepGroupSubmissionWizard_Ext[fieldName].setValue(world.coverageDataMap.get(fieldName))
 }
 
+//Function to select value from coveareg dropdown
 export async function selectDropdown(fieldName) {
     if (world.coverageDataMap.has(fieldName))
         await lOBWizardStepGroupSubmissionWizard_Ext[fieldName].selectOptionByLabel(world.coverageDataMap.get(fieldName))
